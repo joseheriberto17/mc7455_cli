eaa4dca00ba157943e8420064261f59138583a60
joseheriberto17 <jose.heriberto@hotmail.es>
2025-07-03 11:33:51 -0500

    Integracion con la inttefaz para la red movil
    PRESENTE
    - se agregaro algunos comando AT para la RED MOVIL
    - se cambio de puerto USB2 a ttyMODEM

--- cmd/mc7455_cli/main.go
+++ cmd/mc7455_cli/main.go
@@ -20,7 +20,7 @@ func main() {
 		DataBits: 8,
 		StopBits: serial.OneStopBit,
 	}
-	port, err := serial.Open("/dev/ttyUSB2", mode)
+	port, err := serial.Open("/dev/ttyMODEM", mode)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,59 +37,83 @@ func main() {
 		fmt.Print("\033[H\033[2J")
 		fmt.Println("Herramienta de Diagnostico y configuracion de GPS")
 		fmt.Println("\nAcciones especiales")
-		fmt.Println("\t1. Habilitar permisos para comando restringidos")
-		fmt.Println("\t2. Reiniciar el modem")
+		fmt.Println("\t1.  Habilitar permisos para comando restringidos")
+		fmt.Println("\t2.  Realizar un Reinicio completo del modem")
+		fmt.Println("\t3.  Realizar un Reinicio parcial (solo software) del modem")
 
-		fmt.Println("\nEstado del módem y alimentacion:")
-		fmt.Println("\t3. Verificar si el modem esta Operativo (General)")
-		fmt.Println("\t4. Verificar voltaje de alimentacion del modem")
-		fmt.Println("\t5. Verificar temperatura de modem")
+		fmt.Println("\nEstado General del módem y alimentacion:")
+		fmt.Println("\t4.  Verificar si el modem esta Operativo (General)")
+		fmt.Println("\t5.  Verificar voltaje de alimentacion del modem (3,3-4,1 V es OK)")
+		fmt.Println("\t6.  Verificar temperatura de modem")
+		fmt.Println("\t7.  Verificar nivel fucional de modem en general.")
 
-		fmt.Println("\nAlimentación de la antena:")
-		fmt.Println("\t6. Habilitar la alimentacion de modem hacia la antena GPS")
-		fmt.Println("\t7. Verificar la alimentacion de modem hacia la antena GPS")
+		fmt.Println("\nAlimentación de la antena GPS:")
+		fmt.Println("\t8.  Verificar la alimentacion de 3.3V del modem hacia la antena GPS")
+		fmt.Println("\t9.  Habilitar la alimentacion de 3.3V del modem hacia la antena GPS")
+
+		fmt.Println("\nInformacion de la  RED MOVIL:")
+		fmt.Println("\t10.  Verificar el estado general en la RED MOVIL")
+		fmt.Println("\t11. Verificar estado de registro en red de datos (PS)") // cgreg
+		fmt.Println("\t12. Verificar estado de registro en red clásica (CS).") // cgreg
+		fmt.Println("\t13. Muestra la banda o agregación LTE/NR en servicio")
+		fmt.Println("\t14. Lista perfiles PDP (CID, tipo IP, APN)")
 
 		fmt.Println("\nInformacion de GPS")
-		fmt.Println("\t8. Verificar si el GPS del modem esta Operativo")
-		fmt.Println("\t9. Verificar que Capacidades del modem para el GPS estan habilitados")
-		fmt.Println("\t10. Verificar numero de Satélites y su señal SNR")
+		fmt.Println("\t15. Verificar si el GPS del modem esta Operativo")
+		fmt.Println("\t16. Verificar que Capacidades del modem para el GPS estan habilitados")
+		fmt.Println("\t17. Verificar numero de Satélites y su señal SNR")
 
-		fmt.Println("\nRegistros de error")
-		fmt.Println("\t11. ver logs de los errores registrados")
-		fmt.Println("\t12. Borrar logs de los errores registrados")
+		fmt.Println("\nRegistros de errores")
+		fmt.Println("\t18. ver logs de los errores registrados")
+		fmt.Println("\t19. Borrar logs de los errores registrados")
 
 		fmt.Println("\nIniciar fix compatible y monitorizar")
-		fmt.Println("\t13. Iniciar búsqueda GPS")
-		fmt.Println("\t14. Determina posicion final del GPS")
+		fmt.Println("\t20. Iniciar búsqueda GPS (paso 1) ")
+		fmt.Println("\t21. Iniciar búsqueda GPS (paso 2) ")
+		fmt.Println("\t22. Determina posicion final del GPS")
 
-		fmt.Println("\t15. Salir del programa")
+		fmt.Println("\t23. Salir del programa")
 		fmt.Print(">> ")
 
 		input, _ := consoleReader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
-		if input == "15" {
+		if input == "23" {
 			fmt.Println("Saliendo del programa.")
 			break
 		}
 
 		// Mapa de comandos AT
 		atCommands := map[string]string{
-			"1":  "AT!ENTERCND=\"A710\"\r",
-			"2":  "AT!RESET\r",
-			"3":  "AT!PCINFO?\r",
-			"4":  "AT!PCVOLT?\r",
-			"5":  "AT!PCTEMP?\r",
-			"6":  "AT+WANT=1\r",
-			"7":  "AT+WANT?\r",
-			"8":  "AT!GPSSTATUS?\r",
-			"9":  "AT!CUSTOM?\r",
-			"10": "AT!GPSSATINFO?\r",
-			"11": "AT!ERR\r",
-			"12": "AT!ERR=0\r",
-			"13": "AT!GPSEND=0,255\r",
-			"14": "AT!GPSFIX=1,60,10\r",
-			"15": "AT!GPSLOC?\r",
+			// Acciones especiales
+			"1": "AT!ENTERCND=\"A710\"\r",
+			"2": "AT!RESET\r",
+			"3": "AT+CFUN=1,1\r",
+			// Estado General del módem y alimentacion
+			"4": "AT!PCINFO?\r",
+			"5": "AT!PCVOLT?\r",
+			"6": "AT!PCTEMP?\r",
+			"7": "AT+CFUN?\r",
+			// Alimentación de la antena GPS
+			"8": "AT+WANT?\r",
+			"9": "AT+WANT=1\r",
+			// Informacion de la  RED MOVIL:
+			"10": "AT!GSTATUS?\r",
+			"11": "AT+CREG?\r",
+			"12": "AT+CGREG?\r",
+			"13": "AT!GETBAND?\r",
+			"14": "AT+CGDCONT?\r",
+			//informacion de GPS
+			"15": "AT!GPSSTATUS?\r",
+			"16": "AT!CUSTOM?\r",
+			"17": "AT!GPSSATINFO?\r",
+			// Registros de errores
+			"18": "AT!ERR\r",
+			"19": "AT!ERR=0\r",
+			// Iniciar fix compatible y monitorizar
+			"20": "AT!GPSEND=0,255\r",
+			"21": "AT!GPSFIX=1,60,10\r",
+			"22": "AT!GPSLOC?\r",
 		}
 		cmd, ok := atCommands[input]
 
